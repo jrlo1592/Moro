6ee5c36fabb0be8765c161671c79dad41ca65487
BuildTools <jeajohnadams@gmail.com>
2018-05-21 21:43:01 -0500

    Further organization

--- test_build/test.cpp
+++ test_build/test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "math/vector2.hpp"
-#include "physics/collision.hpp"
-#include "graphics/texture.hpp"
+#include "../math/vector2.hpp"
+#include "../physics/collision.hpp"
+#include "../graphics/texture.hpp"
 
 using namespace std;
 
